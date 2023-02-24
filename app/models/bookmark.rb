d4788d40896082c2e4921f3c17889f55cf0e26ac
Carolina Bacelar <carolrbacelar@hotmail.com>
2023-02-24 15:13:26 +0000

    Validations created

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,8 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+
+  # A bookmark must be linked to a movie and a list, and the [movie, list] pairings should be unique.
+  validates :movie_id, uniqueness: { scope: :list_id }
+  validates :comment, length: { minimum: 6 }
 end
